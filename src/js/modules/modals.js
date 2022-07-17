9996a44c426dc5a31ddfd7a233885a564074d631
Igor Stepanov <igorstepanov@hotmail.com>
2022-07-17 15:26:02 +0300

    build(Window): реализовать открытие модального окна через 60 секунд

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -11,28 +11,37 @@ const modals = () => {
         }
 
         modal.style.display = 'block';
-        // document.body.style.overflow = 'hidden';
-        document.body.classList.add('modal-open');
+        document.body.style.overflow = 'hidden';
+        // document.body.classList.add('modal-open');
       });
     });
 
     close.addEventListener('click', () => {
       modal.style.display = 'none';
-      // document.body.style.overflow = '';
-      document.body.classList.remove('modal-open');
+      document.body.style.overflow = '';
+      // document.body.classList.remove('modal-open');
     });
 
     modal.addEventListener('click', (e) => {
       if (e.target === modal) {
         modal.style.display = 'none';
-        // document.body.style.overflow = '';
-        document.body.classList.remove('modal-open');
+        document.body.style.overflow = '';
+        // document.body.classList.remove('modal-open');
       }
     });
   }
 
+  function showModalByTime(selector, time) {
+    setTimeout(() => {
+      document.querySelector(selector).style.display = 'block';
+      document.body.style.overflow = 'hidden';
+    }, time);
+  }
+
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup_engineer .popup_close');
+  showModalByTime('.popup', 60000)
+
 };
 
 export default modals;
