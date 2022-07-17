7935f475ad381eb48ffb125e30ead45521684c86
Igor Stepanov <igorstepanov@hotmail.com>
2022-07-17 17:01:53 +0300

    build(Window): реализовать отправку данных с формы

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
   modals();
   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-  tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+  tabs(
+    '.decoration_slider',
+    '.no_click',
+    '.decoration_content > div > div',
+    'after_click'
+  );
+  forms();
 });
